5714a09c39c452da21e644d61a35810c2d8d51f5
nanjiangshu <nanjiang.shu@gmail.com>
2018-09-07 20:12:30 +0200

    improve the script

--- proj/pred/app/tmp_copy_symlink_to_jobfolder.sh
+++ proj/pred/app/tmp_copy_symlink_to_jobfolder.sh
@@ -2,14 +2,32 @@
 
 # Description: copy the data in cache to this folder, so that seq_origIndex is
 # not a symlink but the folder contains the actual data
+platform_info=`python -mplatform |  tr '[:upper:]' '[:lower:]'`
+platform=
+case $platform_info in 
+    *centos*)platform=centos;;
+    *redhat*) platform=redhat;;
+    *ubuntu*|*debian*)platform=ubuntu;;
+    *)platform=other;;
+esac
+
+
+case $platform in 
+    centos|redhat) user=apache;group=apache;;
+    ubuntu) user=www-data;group=www-data;;
+    other)echo Unrecognized plat form $platform_info; exit 1;;
+esac
 
 filelist=$*
+
+echo $filelist
 for file in $filelist;do
     if [ ! -L $file ];then
         continue
     fi
-    rm -f $file
-    realpath=$(readlink  $file )
+    mv  $file $file.tmp
+    realpath=$(readlink  -f $file.tmp )
     rsync -arz  $realpath/ $file/
-    chown apache:apache -R $file
+    chown $user:$group -R $file
+    rm -f $file.tmp
 done
