ab4ef2932c9560ef5e4b1d21bb6c924df722afe4
nanjiangshu <nanjiang.shu@gmail.com>
2018-10-17 22:28:07 +0200

    update tmp_zip_cached_jobfolder.py so that it can process with provided md5 list file

--- proj/pred/app/tmp_zip_cached_jobfolder.py
+++ proj/pred/app/tmp_zip_cached_jobfolder.py
@@ -27,6 +27,29 @@ else:
     print >> sys.stderr, "Unrecognized platform %s"%(linux_dist)
     sys.exit(1)
 
+def ZipResultFolder(md5_key, cnt):
+    """Zip the result folder
+    """
+    subfoldername = md5_key[:2]
+    md5_subfolder = "%s/%s"%(path_cache, subfoldername)
+    cachedir = "%s/%s/%s"%(path_cache, subfoldername, md5_key)
+    zipfile_cache = cachedir + ".zip"
+    if os.path.exists(cachedir) and not os.path.exists(zipfile_cache):
+        origpath = os.getcwd()
+        os.chdir(md5_subfolder)
+        cmd = ["zip", "-rq", "%s.zip"%(md5_key), md5_key]
+        cmdline = " ".join(cmd)
+        try:
+            print("%d: %s"%(cnt, cmdline))
+            subprocess.check_call(cmd)
+            print("%d: %s"%(cnt, "rmtree(%s)"%(md5_key) ))
+            os.system("chown %s:%s %s"%(user, group, "%s.zip"%(md5_key)))
+            shutil.rmtree(md5_key)
+        except:
+            print >> sys.stderr, "Failed to zip folder %s"%(cachedir)
+            raise
+        os.chdir(origpath)
+
 if __name__ == '__main__':
 
 #metavar='' is the text shown after then option argument
@@ -39,52 +62,42 @@ Created 2018-09-10, updated 2018-09-10, Nanjiang Shu
 Examples:
     %s  -db dbname -exclude-md5 file
 '''%(sys.argv[0]))
-    parser.add_argument('-db' , metavar='DB', dest='db', required=True,
+    parser.add_argument('-db' , metavar='DB', dest='db', required=False,
             help='the name of the finished date db')
-    parser.add_argument('-exclude-md5' , metavar='FILE', dest='exclude_md5_file', required=True,
+    parser.add_argument('-exclude-md5' , metavar='FILE', dest='exclude_md5_file', required=False,
             help='name of the exclude md5 list file')
+    parser.add_argument('-md5' , metavar='FILE', dest='md5_file', required=False,
+            help='name of the include md5 list file')
 
     args = parser.parse_args()
 
     db = args.db
     exclude_md5_file = args.exclude_md5_file
+    md5_file = args.md5_file
 
-    exclude_md5_list = open(exclude_md5_file).read().split("\n")
-    exclude_md5_set = set(exclude_md5_list)
+    if not db == None:
+        exclude_md5_list = open(exclude_md5_file).read().split("\n")
+        exclude_md5_set = set(exclude_md5_list)
+        #print exclude_md5_set
+        tbname_content = "data"
 
-    #print exclude_md5_set
-
-    tbname_content = "data"
-
-    con = sqlite3.connect(db)
-    with con:
-        cur = con.cursor()
-        cmd =  "SELECT md5, seq, date_finish FROM %s"%(tbname_content)
-        cnt = 0
-        for row in cur.execute(cmd):
+        con = sqlite3.connect(db)
+        with con:
+            cur = con.cursor()
+            cmd =  "SELECT md5, seq, date_finish FROM %s"%(tbname_content)
+            cnt = 0
+            for row in cur.execute(cmd):
 #             print row
-            cnt += 1
-            md5_key = row[0]
-            seq = row[1]
-            date_finish = row[2]
-            if not md5_key in exclude_md5_set:
-                subfoldername = md5_key[:2]
-                md5_subfolder = "%s/%s"%(path_cache, subfoldername)
-                cachedir = "%s/%s/%s"%(path_cache, subfoldername, md5_key)
-                zipfile_cache = cachedir + ".zip"
-                if os.path.exists(cachedir) and not os.path.exists(zipfile_cache):
-                    origpath = os.getcwd()
-                    os.chdir(md5_subfolder)
-                    cmd = ["zip", "-rq", "%s.zip"%(md5_key), md5_key]
-                    cmdline = " ".join(cmd)
-                    try:
-                        print("%d: %s"%(cnt, cmdline))
-                        subprocess.check_call(cmd)
-                        print("%d: %s"%(cnt, "rmtree(%s)"%(md5_key) ))
-                        os.system("chown %s:%s %s"%(user, group, "%s.zip"%(md5_key)))
-                        shutil.rmtree(md5_key)
-                    except:
-                        print >> sys.stderr, "Failed to zip folder %s"%(cachedir)
-                        raise
-                    os.chdir(origpath)
+                cnt += 1
+                md5_key = row[0]
+                seq = row[1]
+                date_finish = row[2]
+                if not md5_key in exclude_md5_set:
+                    ZipResultFolder(md5_key, cnt)
+    elif not md5_file == None:
+        md5_list = open(md5_file).read().split("\n")
+        for md5_key in md5_list:
+            ZipResultFolder(md5_key, cnt)
+
+
 
