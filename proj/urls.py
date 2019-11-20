33c7d5c6f77b27f5d5dc949ed9e19c689a348511
nanjiangshu <nanjiang.shu@gmail.com>
2019-11-20 22:10:56 +0100

    fix url in main proj

--- proj/urls.py
+++ proj/urls.py
@@ -23,7 +23,7 @@ from proj import views
 from proj import pred
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    path('^admin/', admin.site.urls),
     url(r'^pred/', include('proj.pred.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')), # new
     url(r'^$', views.home, name='home'),
