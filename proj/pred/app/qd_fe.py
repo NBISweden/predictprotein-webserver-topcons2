02c55b4bfbeeed424f3f2a442e71a9340df9b907
nanjiangshu <nanjiang.shu@gmail.com>
2019-11-26 14:37:30 +0100

    chanage seperator

--- proj/pred/app/qd_fe.py
+++ proj/pred/app/qd_fe.py
@@ -1979,6 +1979,6 @@ def InitGlobalParameter():#{{{
 if __name__ == '__main__' :
     g_params = InitGlobalParameter()
     date_str = time.strftime(g_params['FORMAT_DATETIME'])
-    print("\n#===============#\n[Date: %s] qd_fe.py restarted"%(date_str))
+    print("\n#%s#\n[Date: %s] qd_fe.py restarted"%('='*80,date_str))
     sys.stdout.flush()
     sys.exit(main(g_params))
