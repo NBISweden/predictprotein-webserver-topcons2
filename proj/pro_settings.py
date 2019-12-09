d8ddda035190c5f917f8e6beb6635b9deee2d11e
nanjiangshu <nanjiang.shu@gmail.com>
2019-12-09 15:09:33 +0100

    define ALLOWED_HOSTS

--- proj/pro_settings.py
+++ proj/pro_settings.py
@@ -2,19 +2,17 @@
 Django settings for proj project in production
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
+https://docs.djangoproject.com/en/2.2.7/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
+https://docs.djangoproject.com/en/2.2.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import sys
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 from libpredweb import myfunc
 from libpredweb import webserver_common as webcom
-
 try:
     from .shared_settings import *
 except ImportError:
@@ -23,9 +21,11 @@ except ImportError:
 with open('/etc/django_pro_secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+ALLOWED_HOSTS = []
 allowed_host_file = "%s/allowed_host_pro.txt"%(BASE_DIR)
 computenodefile = "%s/pred/config/computenode.txt"%(BASE_DIR)
 for f in [allowed_host_file, computenodefile]:
--- proj/shared_settings.py
+++ proj/shared_settings.py
@@ -130,5 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "%s/pred/static"%(BASE_DIR)
 SUPER_USER_LIST = ["admin","nanjiang", "njshu"]
 
