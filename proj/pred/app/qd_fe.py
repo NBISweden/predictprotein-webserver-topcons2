100c15ba9f0026fedc8cc634842238cb74a4809b
nanjiangshu <nanjiang.shu@gmail.com>
2020-05-01 15:40:40 +0200

    set timeout for urlretrieve to 10 seconds

--- proj/pred/app/qd_fe.py
+++ proj/pred/app/qd_fe.py
@@ -1057,7 +1057,7 @@ def GetResult(jobid):#{{{
                         jobid, origIndex, result_url), gen_logfile, "a", True)
                     if myfunc.IsURLExist(result_url,timeout=5):
                         try:
-                            urllib.urlretrieve (result_url, outfile_zip)
+                            myfunc.urlretrieve (result_url, outfile_zip, timeout=10)
                             isRetrieveSuccess = True
                             myfunc.WriteFile(" succeeded on node %s\n"%(node), gen_logfile, "a", True)
                         except:
