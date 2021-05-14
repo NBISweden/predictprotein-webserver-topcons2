5b8db104d24c4d8a6670897b6c13bdc51dd7ec96
nanjiangshu <nanjiang.shu@gmail.com>
2021-05-14 23:44:17 +0200

    feat: when runjob.lock does not exist, qd_fe should take over, set UPPER_WAIT_TIME_IN_SEC to 0

--- proj/pred/app/qd_fe.py
+++ proj/pred/app/qd_fe.py
@@ -250,7 +250,7 @@ def CreateRunJoblog(path_result, submitjoblogfile, runjoblogfile,#{{{
 
             # single-sequence job submitted from the web-page will be
             # submmitted by suq
-            UPPER_WAIT_TIME_IN_SEC = 60
+            UPPER_WAIT_TIME_IN_SEC = 0
             isValidSubmitDate = True
             try:
                 submit_date = webcom.datetime_str_to_time(submit_date_str)
