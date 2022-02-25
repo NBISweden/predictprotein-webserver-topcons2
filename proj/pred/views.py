247a3e674b8ac198b7757f98c92b216b339862d5
Cloud User <cloud-user@dev-web.egi.eu>
2022-02-25 14:39:43 +0100

    feat: exploring graph.js for server statistics graph

--- proj/pred/views.py
+++ proj/pred/views.py
@@ -495,6 +495,17 @@ def help_wsdl_api(request):# {{{
 def get_serverstatus(request):# {{{
     g_params['isShowLocalQueue'] = False
     info = webcom.get_serverstatus(request, g_params)
+    #TODO: Write function that parse stat file for day, week, month, year
+    file_path = '/software/server/data/topcons_stat_files/stat/submit_month_web.stat.txt'
+    server_stat_data = []
+    with open(file_path) as f:
+        for line in f:
+            if not line.startswith('Date'):
+                sline = line.strip('\n')
+                cols = sline.split('\t')
+                server_stat_data.append(cols)
+    #TODO: Have variable for day, week month and year respectively?
+    info['server_stat_month'] = json.dumps(server_stat_data)
     return render(request, 'pred/serverstatus.html', info)
 # }}}
 
