37921f174a5ca1283632151cd2c722a2d76b17aa
MalinAhlberg <malin.ahlberg@nbis.se>
2022-03-08 10:29:39 +0100

    chore: add docker settings

--- dockerrun.sh
+++ dockerrun.sh
@@ -2,5 +2,5 @@
 mkdir -p /topcons/proj/pred/static/log
 touch /topcons/proj/pred/static/log/debug.log
 cd /topcons
-cp proj/dev_settings.py proj/settings.py
+cp proj/docker_settings.py proj/settings.py
 python3 /topcons/manage.py runserver 8000
--- proj/wsgi.py
+++ proj/wsgi.py
@@ -11,13 +11,16 @@ import os
 import sys
 import site
 
+import proj.settings
+
 rundir = os.path.dirname(os.path.abspath(__file__))
 basedir = os.path.abspath("%s/../"%(rundir))
 path_log = "%s/pred/static/log"%(rundir)
 
 # Activate the virtual env
-activate_env="%s/env/bin/activate_this.py"%(basedir)
-exec(compile(open(activate_env, "r").read(), activate_env, 'exec'), dict(__file__=activate_env))
+if proj.settings.USE_ENV:
+    activate_env="%s/env/bin/activate_this.py"%(basedir)
+    exec(compile(open(activate_env, "r").read(), activate_env, 'exec'), dict(__file__=activate_env))
 os.system("echo which python; which python >> %s/debug.log" %( path_log) )
 
 #Add the site-packages of the virtualenv
